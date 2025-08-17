9f7d5023b363f555092dbee3a260d852a2e6a2b4
LexSviatov <lexsviatov@gmail.com>
2025-08-17 19:49:06 +0700

    auto-commit: 17.08.2025, 19:49:06

--- nodejs/ecosystem.config.js
+++ nodejs/ecosystem.config.js
@@ -1,43 +1,35 @@
-module.exports = {
-  apps: [
-    {
-      name: 'alls',
-      script: 'D:/Gitlab/my-crm/nodejs/alls.js',
-      watch: true,
-      autorestart: true
-    },
-    {
-      name: 'start',
-      script: 'D:/Gitlab/my-crm/nodejs/index.js',
-      watch: true,
-      autorestart: true
-    },
-    {
-      name: 'auto-commits',
-      script: 'D:/Projects/Work/myproject-scripts/auto-commit.js',
-      cwd: 'D:/Projects/Work/myproject-scripts',
-      watch: false,
-      autorestart: true  // включаем авто-перезапуск
-      // убрали cron_restart, вместо этого используем setInterval внутри скрипта
-    },
-    {
-      name: 'branch-watcher',
-      script: 'D:/Projects/Work/myproject-scripts/branch-watcher.js',
-      cwd: 'D:/Projects/Work/myproject',
-      watch: false,
-      autorestart: true
-    }
-  ],
-  deploy: {
-    production: {
-      user: 'SSH_USERNAME',
-      host: 'SSH_HOSTMACHINE',
-      ref: 'origin/master',
-      repo: 'GIT_REPOSITORY',
-      path: 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
+apps: [
+  {
+    name: 'alls',
+    script: 'D:/Gitlab/my-crm/nodejs/alls.js',
+    watch: true,
+    autorestart: true
+  },
+  {
+    name: 'start',
+    script: 'D:/Gitlab/my-crm/nodejs/index.js',
+    watch: true,
+    autorestart: true
+  },
+  {
+    name: 'auto-commits',
+    script: 'D:/Projects/Work/myproject-scripts/auto-commit.js',
+    cwd: 'D:/Projects/Work/myproject-scripts',
+    watch: false,
+    autorestart: true
+  },
+  {
+    name: 'branch-watcher',
+    script: 'D:/Projects/Work/myproject-scripts/branch-watcher.js',
+    cwd: 'D:/Projects/Work/myproject',
+    watch: false,
+    autorestart: true
+  },
+  {
+    name: 'auto-commit-watcher',
+    script: 'D:/Projects/Work/myproject-scripts/auto-commit-watcher.js',
+    cwd: 'D:/Projects/Work/myproject-scripts',
+    watch: false,
+    autorestart: true
   }
-};
+]
