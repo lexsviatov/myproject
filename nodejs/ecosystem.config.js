65fe824390df25eedb275f5cc87915321d635e3d
LexSviatov <lexsviatov@gmail.com>
2025-08-19 19:14:55 +0700

    Auto commit

--- nodejs/ecosystem.config.js
+++ nodejs/ecosystem.config.js
@@ -1,35 +1,44 @@
-apps: [
-  {
-    name: 'alls',
-    script: 'D:/Gitlab/my-crm/nodejs/alls.js',
-    watch: true,
-    autorestart: true
-  },
-  {
-    name: 'start',
-    script: 'D:/Gitlab/my-crm/nodejs/index.js',
-    watch: true,
-    autorestart: true
-  },
-  {
-    name: 'auto-commits',
-    script: 'D:/Projects/Work/myproject-scripts/auto-commit.js',
-    cwd: 'D:/Projects/Work/myproject-scripts',
-    watch: false,
-    autorestart: true
-  },
-  {
-    name: 'branch-watcher',
-    script: 'D:/Projects/Work/myproject-scripts/branch-watcher.js',
-    cwd: 'D:/Projects/Work/myproject',
-    watch: false,
-    autorestart: true
-  },
-  {
-    name: 'auto-commit-watcher',
-    script: 'D:/Projects/Work/myproject-scripts/auto-commit-watcher.js',
-    cwd: 'D:/Projects/Work/myproject-scripts',
-    watch: false,
-    autorestart: true
-  }
-]
+module.exports = {
+  apps: [
+    {
+      name: 'alls',
+      script: 'D:/Gitlab/my-crm/nodejs/alls.js',
+      watch: true,
+      autorestart: true
+    },
+    {
+      name: 'start',
+      script: 'D:/Gitlab/my-crm/nodejs/index.js',
+      watch: true,
+      autorestart: true
+    },
+    {
+      name: 'auto-commits',
+      script: 'D:/Projects/Work/myproject-scripts/auto-commit.js',
+      cwd: 'D:/Projects/Work/myproject-scripts',
+      watch: false,
+      autorestart: true
+    },
+    {
+      name: 'branch-watcher',
+      script: 'D:/Projects/Work/myproject-scripts/branch-watcher.js',
+      cwd: 'D:/Projects/Work/myproject',
+      watch: false,
+      autorestart: true
+    },
+    {
+      name: 'auto-commit-watcher',
+      script: 'D:/Projects/Work/myproject-scripts/auto-commit-watcher.js',
+      cwd: 'D:/Projects/Work/myproject-scripts',
+      watch: false,
+      autorestart: true
+    },
+    {
+      name: 'myproject-app',
+      script: 'index.js',
+      cwd: 'D:/Projects/Work/myproject/nodejs',
+      watch: true,
+      autorestart: true
+    }
+  ]
+}
